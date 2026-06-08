b9cbc36c6a16c4239cbd42cde57e20e60e8ed949
komalll6 <komalkalra698@gmail.com>
2026-06-08 20:37:45 +0530

    first commit

---
+++ datatype.js
@@ -0,0 +1,52 @@
+//JS- Interpreter language
+//in char give - string Datatype
+//DECLARE using 3 types-  let, var, char
+//VARIABLE- storage space
+
+//let- redeclare
+let name = "ABC" //words are in "" or '' are- string DT
+
+name ="Komal Kalra"
+console.log(name)
+
+//const - cannot change
+const pi = 4.21;
+
+//var - fuction scope, not used in modern language
+var age = 21;
+
+console.log(typeof name) 
+
+//NUMBERS
+let marks = 87 //not write  in "" or ''
+console.log(typeof marks)
+
+//BOOLEAN or BOOL- have two value- true or false ie, 0 or 1
+let isLoggedin = false
+console.log(typeof isLoggedin)
+
+//undefine datatype
+let country 
+console.log(typeof country) //undefine bcoz it only container not any store value 
+
+//NULL datatype
+let school = null
+console.log(school) //if user loggedin thenn it give value
+//THESE ALL ARE PRIMITIVE DATATYPE.
+
+
+// //task
+// let name = "KOMAL KALRA"
+// console.log(name)
+
+// let collage = ("LKCTC")
+// console.log(collage)
+
+// let city = ("Jalandhar")
+// console.log(city)
+
+// let state = ("Punjab")
+// console.log(state)
+
+// console.log("hi my name is: " + name + "and im from" + collage + city + state) 
+// //task- 
---
+++ task.js
@@ -0,0 +1,14 @@
+//Task 
+let name = "KOMAL KALRA"
+// console.log(name)
+
+let collage = ("LKCTC")
+// console.log(collage)
+
+let city = ("Jalandhar")
+// console.log(city)
+
+let state = ("Punjab")
+// console.log(state)
+
+console.log("Hi My Name is: " + name + " and I'm from " + collage + city + state )
