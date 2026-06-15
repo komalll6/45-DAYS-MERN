df2cd21fc5b6e0d0714b59d2ef368aca56d29cc2
komalll6 <komalkalra698@gmail.com>
2026-06-15 22:35:54 +0530

    another commit

--- task-8.js
+++ task-8.js
@@ -1,8 +1,8 @@
 //define logic- to search the index of array
 let array=[2,4,3,6,7,9,5,23,0] 
 function searchElements(arr, elt){
-    for(let i = 0; i<=hello.length;i++){
-        if (arr[i] == elt){
+    for(let i = 0; i<=arr.length;i++){
+        if (arr[i] = elt){
             console.log("array are found at index: ", i)
         }
     }
