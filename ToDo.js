5197af87a19e38f471dc25e6e0ec9ae900c5e905
komalll6 <komalkalra698@gmail.com>
2026-06-19 10:46:14 +0530

    ToDo APPLICATION commit

--- ToDo.js
+++ ToDo.js
@@ -6,13 +6,16 @@ function addTask(){
     let task = document.querySelector("#task-input").value
     let date = document.querySelector("#date-input").value
 
+if (!task || !date){
+    alert("please fill the task and date")
+    return
+}
     todos.push({
         taskname : task,
         taskdate : date,
     })
 
     localStorage.setItem("todo", JSON.stringify(todos)) //store on local storage even the page are refresh, "JSON.stringify" to store 
-
     display()
 
     document.querySelector("#task-input").value = ""
@@ -20,16 +23,24 @@ function addTask(){
 
 }   
 
-function display(){
+function display(){ //sensation@12#
     let result = ""
 
     for(let i=0; i<todos.length; i++){
         result +=
         `<span> 
         ${todos[i].taskname} - ${todos[i].taskdate}
+
+        <button onClick="deleteTask(${i})">Delete</button> 
+        <button onClick="clearTask">Clear</buttun>
         </span> <br>`;
 
         document.querySelector("#list").innerHTML = result
-        
         }
+}
+
+function deleteTask(index){
+    todos.splice(index,1)
+    localStorage.setItem("todo", JSON.stringify(todos)) //store on local storage even the page are refresh, "JSON.stringify" to store 
+    display()
 }
