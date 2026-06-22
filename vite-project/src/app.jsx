51e3013445e4e7afac5f76fd990848e6dd818b70
komalll6 <komalkalra698@gmail.com>
2026-06-22 10:58:15 +0530

    NEW commit USING REACT-SETUP

---
+++ vite-project/src/app.jsx
@@ -0,0 +1,19 @@
+function App(){
+
+  let name = "koml kalra"
+
+   return(
+    <>
+    <h1>HELLOOOS</h1> 
+    <h2>Hi, {name}</h2>
+    <p>Lorem ipsum dolor sit amet consectetur 
+      adipisicing elit. Illum quos cum sapiente 
+      odit aliquid nostrum iure provident ducimus 
+      consequuntur libero officiis doloremque, 
+      aliquam enim maiores ipsam fugit ex nobis? 
+      Obcaecati.</p>
+    </>
+   )
+}
+
+export default App
---
+++ vite-project/src/main.jsx
@@ -0,0 +1,8 @@
+import './index.css'
+import App  from './app.jsx'
+
+createroot(document.getElementById('root')).render(
+<script>
+<App/>
+</script>
+)
