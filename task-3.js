c5a6ac2b69e581dc1e6ef7ae17bd1bc7a967cc2e
komalll6 <komalkalra698@gmail.com>
2026-06-23 22:09:48 +0530

    updated the task

--- task-3.js
+++ task-3.js
@@ -1,9 +1,26 @@
 //TASK3- take eg of amount and in particular amnt. gives this discount.
-let amnt = `20,000`
-if(amnt > `19,239`){
-    console.log("Giving you itna discount")
-}
+// let amnt = `20,000`
+// if(amnt > `19,239`){
+//     console.log("Giving you itna discount")
+// }
 
-else if(amnt >= `12,483` && amnt < `32,494`){
-    console.log("Biggest amnt")
+// else if(amnt >= `12,483` && amnt < `32,494`){
+//     console.log("Biggest amnt")
+// }
+
+// TASK3: Fixed code that shows the amount in the console output
+
+let amnt = 20000; 
+
+if (amnt >= 32494) {
+    console.log(`Amount is ${amnt}: Biggest amnt!`);
+} 
+else if (amnt > 19239) {
+    console.log(`Amount is ${amnt}: Giving you itna discount`);
+} 
+else if (amnt >= 12483) {
+    console.log(`Amount is ${amnt}: Giving you a smaller discount`);
+} 
+else {
+    console.log(`Amount is ${amnt}: No discount available`);
 }
--- task-6.js
+++ task-6.js
@@ -1,17 +1,41 @@
 // square of number with functions
-function square(num){
-    if(num*2 ){
-        console.log("square")
-    }
+// function square(num){
+//     if(num*2 ){
+//         console.log("square")
+//     }
 
-    else {
-        console.log("not square")
-    }
+//     else {
+//         console.log("not square")
+//     }
+// }
+
+// square(20)
+// function square(num){
+//     console.log("")
+// }
+
+// square(4)
+
+// function sqr(num){
+//     let result = num*num
+//     return result;
+// }
+
+// let num = 20
+// console.log(num)
+
+
+// Task: Calculate the square of a number using a function
+
+// 1. We define the function machine
+function findSquare(num) {
+    let result = num * num; 
+    return result; // "return" sends the final answer out of the machine
 }
 
-square(20)
-function square(num){
-    console.log("")
-}
+// 2. We use (call) the function and save the answer
+let originalNumber = 6;
+let squaredNumber = findSquare(originalNumber);
 
-square(4)
+// 3. Print the output using a template literal
+console.log(`The square of ${originalNumber} is: ${squaredNumber}`);
