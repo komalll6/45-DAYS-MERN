0c5696795e45256a616198ae10bc43df5bce6485
komalll6 <komalkalra698@gmail.com>
2026-07-03 11:11:11 +0530

    new commit with set-up of Crud-api

--- crud-api/node.js
+++ crud-api/node.js
@@ -1,60 +1,49 @@
-//to use exprees =  > first install express using npm install express --save
-//get => to fetch data from server
-//res => backend response to the frontend
-//req => what the request is coming from the frontend
-//res.json => to send json data to the frontend
-
 //Postman => api bnayegy toh test kregy postman pr and check the response
 
 import express from 'express'; //import express module
 
 const app = express(); //create an express app => it is extense
 
+app.use(express.json()) //middleware => to parse json data from frontend to backend
+//express.json => inbuilt middleware function in express.
+//api => application programming interface.
+//api => it is a set of rules that allows different software applications to communicate with each other.
+//types of api => 1. get => to get data from backend to frontend
+//2. post => to send data from frontend(add student) to backend
+//3. put => to update data from frontend to backend / to change data from frontend to backend
+//4. delete => to delete data from backend to frontend
 
-//MIDDLEWARE => user login hai ya ni
-app.use((req,res,next) =>{
-    console.log("this is middleware")
-    next() //jb wo chiz run kr jayegi toh next to control chla jayaga, agr next middle ware hai  toh next pr chla jaye
-})
+const students = [ //array of objects => to store different data of students
+    {
+        id: 1,
+        name: "John Doe",
+        class: 20,
+        city: "New York",
+        school: "ABC School"
+    },
+   {
+        id: 2,
+        name: "Jane Smith",
+        class: 22,
+        city: "Los Angeles",
+        school: "XYZ School"
+    }
+] //go to postman and check the response of get api => http://localhost:3001/students
+//change to get and test the api in test block then check the response in postman => it will show the data of students in json format
 
+app.get("/students",(req,res) =>{  //get api => to get data from backend to frontend
+    res.json(students)  //send response to frontend
+}) 
 
-
-//this is routes
-app.get('/', (req, res) => {
-    // res.send("WORKING") //send response to the frontend
-    res.send("<h1>Hello, World!</h1>")
-})
-
-app.get('/student', (req, res) => {
-    res.json({name:"abc", age:22, city:"jalandhar cantt"}) //normally use this
-})
-
-//change the route or url to localhost:3001/student/1 => 1 is id
-app.get('/student/:id', (req, res) => {
-
-    res.json(console.log(req.params.id)) //use when we search for a specific student by id, name, age, city etc
-
-    res.json(console.log(req.query)) //use when we use filter or search for a specific student by name or age or city etc
-
-//()
-
-    // res.json({name:"abc", age:22, city:"jalandhar cantt"}) //normally use this
-})
-
-app.get('/profile', (req, res) => {
-    res.status(200).send("this is profile page") //normally use this
-})
-//agr koi error hai ya glt hai toh wo code return krega
-
-app.get('/status', (req, res) => {
-    res.send("this is status page") //normally use this
-    res.json({"status":"ok", "message":"Everything is working fine"}) //normally use this
+app.post("/students", (req, res) => {  //go to postman and change GET to POST and run the code
+    students.push({id:3, name:"newname", age:22})
+    res.json({
+        message:"Student added successfully", students
+    })
 })
 
 app.listen(3001, () =>{
     console.log("server is running")
 })
-
-app.listen(3001)
 
 //insepct => network => header =>status code => 200 => means everything is working fine
