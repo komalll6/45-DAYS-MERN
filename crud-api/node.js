af792dffc41b55bbecfe70fad19020dcd3c24d83
komalll6 <komalkalra698@gmail.com>
2026-07-06 10:27:17 +0530

    new commit with set-up of Crud-api

--- crud-api/node.js
+++ crud-api/node.js
@@ -33,14 +33,46 @@ const students = [ //array of objects => to store different data of students
 
 app.get("/students",(req,res) =>{  //get api => to get data from backend to frontend
     res.json(students)  //send response to frontend
-}) 
+})  
 
 app.post("/students", (req, res) => {  //go to postman and change GET to POST and run the code
     students.push({id:3, name:"newname", age:22})
+
+//06-07-26
+    students.push(req.body) //push the data from frontend to backend => req.body => it is the data sent from frontend to backend
+    //POST =>BODY => raw => json => { "id": 4, "name": "newname", "age": 22 } => send => check the response in postman
+
     res.json({
         message:"Student added successfully", students
     })
 })
+
+//TO UPDATE DATA => PUT API => to update data from frontend to backend
+app.put("/students/:id", (req, res) => { //go to postman and change GET to PUT and run the code
+  const studentId = Number(req.params.id, 10)
+
+    const index = students.findIndex(s => s.id === studentId) 
+
+  students[index] = req.body //update the data from frontend to backend => req.body => it is the data sent from frontend to backend
+  res.json({
+    message:"Student updated successfully", students
+   })
+})
+//PUT =>BODY => raw => json => { "id": 4, "name": "newname", "age": 22 } => send => check the response in postman   
+//http://localhost:3001/students/2 => /2 coz new id di hai
+
+
+//delete operation => delete api => to delete data from backend to frontend
+app.delete("/students/:id", (req, res) => { //go to postman and change GET to DELETE and run the code
+
+    const studentId = Number(req.params.id, 10)
+
+    const new_students = students.filter((s) => s.id !== req.params.id)
+    res.json({
+        message:"student deleted sucessfully",
+        new_students, //students array will return
+    })
+}) //delete => body => raw => JSON 
 
 app.listen(3001, () =>{
     console.log("server is running")
