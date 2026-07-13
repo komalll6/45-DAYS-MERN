50f103f2c0f77ceeeb0d5750b289d9a767300c23
komalll6 <komalkalra698@gmail.com>
2026-07-13 23:54:37 +0530

    COMMITED with backend

--- API-REVISE/ROUTER/auth.router.js
+++ API-REVISE/ROUTER/auth.router.js
@@ -3,5 +3,7 @@ import express from "express"
 const router = express.Router()
 
 router.post("/signup", signup)
+router.post("/signin", signin)
+
 
 export default router
--- API-REVISE/controller/auth.controller.js
+++ API-REVISE/controller/auth.controller.js
@@ -34,3 +34,40 @@ export const signup = async(req, res, next)=>{
     })
   }
 }
+
+
+//signin
+export const signin = async (req, res, next) =>{
+    try{
+        const {email, password} = req.body
+
+        if(!email || !password) {
+            return res.send(400).json({
+                message: "please fill all fields"
+            })
+        }
+        const user = await Auth.findOne({email})
+
+        if(!user){
+           return res.status(404).json({
+            message: "user not found"
+           })
+        }
+
+        if (User.password !== password){
+            return res.status(401).json({
+                message: "Incorrect password",
+            })
+            return res.status(201).json({
+                message: "user login sucessful",
+                data: {
+                    _id: user._id,
+                    email: user.email,
+                },
+            })
+        } catch (err){
+            return res.status(500).json({
+                message: err.message,
+            })
+        }
+    }        
